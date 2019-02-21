735fa27c87cc0958cd5b57aba7ab11a5890eb599
jppiazza <johnpaulpiazza@gmail.com>
2019-02-21 11:53:33 -0500

    Create open3dtest.py

---
+++ Sample PCD Files/open3dtest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 17 20:21:39 2019
+
+@author: johnp
+"""
+
+from open3d import *
+
+pcd = read_point_cloud("./test_pcd/1547842929.701970000.pcd")
+#print(pcd)
+ #   print(np.asarray(pcd.points))
+draw_geometries_with_editing([pcd])
