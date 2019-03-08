5e3cd3ed8b3e243ce2a09f73731ed14cca05d5eb
pqmzla1029 <igormonteiro101@gmail.com>
2019-03-08 16:25:36 -0500

    WORKS!!!!!!

--- Test/tkinter_trial1.py
+++ Test/tkinter_trial1.py
@@ -11,10 +11,17 @@ fig,ax = plt.subplots(1)
 # Display the image
 ax.imshow(im)
 
+# List to hold x values.
+x_number_values = [463, 753, 988, 851, 463, 463, 753, 988, 851, 463]
+
+# List to hold y values.
+y_number_values = [930, 972, 972, 930, 930, 1332, 1215 ,1215, 1332, 1332]
+
 # Create a Rectangle patch
-rect = patches.Rectangle((50,100),40,30,linewidth=1,edgecolor='r',facecolor='none')
+#rect = patches.Rectangle((463,930),(930-463),971,linewidth=1,edgecolor='r',facecolor='none')
+plt.plot(x_number_values, y_number_values, linewidth=3)
 
 # Add the patch to the Axes
-ax.add_patch(rect)
+#ax.add_patch(rect)
 
 plt.show()
