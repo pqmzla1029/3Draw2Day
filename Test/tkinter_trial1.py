78928ede7afaebc47af5d6286f6655625a2f9a36
pqmzla1029 <igormonteiro101@gmail.com>
2019-03-08 16:03:31 -0500

    sfdf

--- Test/tkinter_trial1.py
+++ Test/tkinter_trial1.py
@@ -1,100 +1,20 @@
-from tkinter import *
-from tkinter.colorchooser import askcolor
-from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+import numpy as np
 
-class Paint(object):
+im = np.array(Image.open('frame0000.jpg'), dtype=np.uint8)
 
-    DEFAULT_PEN_SIZE = 5.0
-    DEFAULT_COLOR = 'black'
+# Create figure and axes
+fig,ax = plt.subplots(1)
 
-    def __init__(self):
-        self.root = Tk()
+# Display the image
+ax.imshow(im)
 
-        self.pen_button = Button(self.root, text='Select', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+# Create a Rectangle patch
+rect = patches.Rectangle((50,100),40,30,linewidth=1,edgecolor='r',facecolor='none')
 
-        self.brush_button = Button(self.root, text='Add', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+# Add the patch to the Axes
+ax.add_patch(rect)
 
-        self.color_button = Button(self.root, text='Delete', command=self.choose_color)
-        self.color_button.grid(row=0, column=2)
-
-        self.eraser_button = Button(self.root, text='Confirm', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
-
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
-
-        self.rectangle_button = Button(self.root, text='Bound Auto', command=self.draw_rectangle)
-        self.rectangle_button.grid(row=0, column=5)
-        
-        image = ImageTk.PhotoImage(file ="maxresdefault.jpg")
-        self.c = Canvas(self.root, width=1000, height=1000)
-        self.c.create_image(10, 10, image = image, anchor = NW)
-        self.c.grid(row=1, columnspan=6)
-
-        self.setup()
-        #self.initUI()
-        self.root.mainloop()
-
-    def initUI(self):
-
-
-        self.pack(fill=BOTH, expand=1)
-
-        self.img = Image.open("car.jpg")
-        self.tatras = ImageTk.PhotoImage(self.img)
-
-        canvas = Canvas(self, width=self.img.size[0]+20,
-           height=self.img.size[1]+20)
-        canvas.create_image(10, 10, anchor=NW, image=self.tatras)
-        canvas.pack(fill=BOTH, expand=1)
-
-    def draw_rectangle(self):
-        self.c.create_rectangle(230, 120, 290, 170,outline="#f11", width=2)
-
-    def setup(self):
-        self.old_x = None
-        self.old_y = None
-        self.line_width = self.choose_size_button.get()
-        self.color = self.DEFAULT_COLOR
-        self.eraser_on = False
-        self.active_button = self.pen_button
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
-
-    def use_pen(self):
-        self.activate_button(self.pen_button)
-
-    def use_brush(self):
-        self.activate_button(self.brush_button)
-
-    def choose_color(self):
-        self.eraser_on = False
-        self.color = askcolor(color=self.color)[1]
-
-    def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
-
-    def activate_button(self, some_button, eraser_mode=False):
-        self.active_button.config(relief=RAISED)
-        some_button.config(relief=SUNKEN)
-        self.active_button = some_button
-        self.eraser_on = eraser_mode
-
-    def paint(self, event):
-        self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=paint_color,
-                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
-        self.old_x = event.x
-        self.old_y = event.y
-
-    def reset(self, event):
-        self.old_x, self.old_y = None, None
-
-
-if __name__ == '__main__':
-    Paint()
+plt.show()
