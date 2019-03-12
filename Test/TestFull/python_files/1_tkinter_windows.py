52ac0029fd67cd964bcad7a44894bfbb4f25dd68
jppiazza <johnpaulpiazza@gmail.com>
2019-03-12 16:24:56 -0400

    Editted the instructions

--- Test/TestFull/python_files/1_tkinter_windows.py
+++ Test/TestFull/python_files/1_tkinter_windows.py
@@ -6,11 +6,11 @@ import os
 #print(os.getcwd())
 class MyFirstGUI:
     LABEL_TEXT = [
-        "Main Screen \n Help \n 1. Z - Lock in z-axis \n 2. K - Lock for cropping \n 3. Draw Bounding Box \n 4. C - Save(Enter) \n X - Lock in x-axis \n 2. K - Lock for cropping \n 3. Draw Bounding Box \n 4. C - Save(Enter) \n Q - Quit",
-        "Actually, this is our second GUI.",
-        "We made it more interesting...",
-        "...by making this label interactive.",
-        "Go on, click on it again.",
+        "Instructions \n\n 1. Press Z to lock the z-axis \n 2. Press K to lock for cropping \n 3. Draw your 2D bounding box using the mouse \n 4. Press C, then Enter, to save the crop \n 5. Press X to lock the x-axis \n 6. Press to lock for cropping \n 7. Draw your 2D bounding box using the mouse \n 8. Press C, then Enter, to save the crop \n 9. Press Escape \n 10. Press 'Proceed' \n",
+        # "Actually, this is our second GUI.",
+        # "We made it more interesting...",
+        # "...by making this label interactive.",
+        # "Go on, click on it again.",
     ]
     def __init__(self, master):
         self.master = master
