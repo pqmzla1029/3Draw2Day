ad4995ae373e36404fa476a852ac004ed7402d13
zhaxiche <zhaxiche@gmail.com>
2019-03-13 13:42:37 -0400

    Update 3_json_read.py

--- Test/TestFull - Copy (2)/python_files/3_json_read.py
+++ Test/TestFull - Copy (2)/python_files/3_json_read.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 #import objectpath
 import os
+from open3d import *
 print(os.getcwd())
 
 with open("working_data/json_crops/cropped_1.json", "r") as wow:
@@ -23,8 +24,12 @@ random_array=json_string["bounding_polygon"]
 myarray3 = np.asarray(random_array)
 #print(myarray2[:,2])
 
-myarray1[:,2]=myarray3[0,2]
-myarray2[:,2]=myarray3[2,2]
+with open("working_data/ply_crops/cropped_1.ply","r") as okok:
+pcd = read_point_cloud(okok)
+#myarray1[:,2]=myarray3[0,2]
+myarray1[:,2] = pcd.get_min_bound()
+myarray2[;,2] = pcd.get_max_bound()
+#myarray2[:,2]=myarray3[2,2]
 #print(myarray1)
 
 f= open("working_data/bounding_data/bound_data.txt","w+")
