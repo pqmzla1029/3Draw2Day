ec1d53d8752b03a901e52f92954fa598c8063422
pqmzla1029 <igormonteiro101@gmail.com>
2019-03-13 19:36:32 -0400

    sMALL CHANGES

--- Test/TestFull - Copy (2)/run_full.bat
+++ Test/TestFull - Copy (2)/run_full.bat
@@ -1,6 +1,7 @@
-python python_files\1_tkinter_windows.py
-python python_files\2_copy_data.py
-python python_files\3_json_read.py
-python python_files\4_convert_to_image_frame.py
-python python_files\5_view_conversion.py
+cd python_files
+python 1_tkinter_windows.py
+python 2_copy_data.py
+python 3_json_read.py
+python 4_convert_to_image_frame.py
+python 5_view_conversion.py
 pause
