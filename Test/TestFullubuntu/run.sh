18f2d64242b41cc8f0609e96ef1c16be3652b60b
pqmzla1029 <igormonteiro101@gmail.com>
2019-04-17 18:16:18 -0400

    Final update on this for today

--- Test/TestFullubuntu/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 python_files/test_gui_full.py
--- Test/TestFullubuntu/run_full.bat
+++
@@ -1,6 +0,0 @@
-python python_files\1_tkinter_windows.py
-python python_files\2_copy_data.py
-python python_files\3_json_read.py
-python python_files\4_convert_to_image_frame.py
-python python_files\5_view_conversion.py
-pause
